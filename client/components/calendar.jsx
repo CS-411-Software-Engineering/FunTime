b25a20d3474e8eea2cb3365c4fbfd6e5273ec004
toutou826 <37494307+toutou826@users.noreply.github.com>
2019-10-23 14:56:28 -0400

    added start time and end time in calendar

--- client/components/calendar.jsx
+++ client/components/calendar.jsx
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
-import {Row, Col} from 'react-bootstrap';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import BigCalendar from 'react-big-calendar';
-import moment from 'moment';
+// import {Row, Col} from 'react-bootstrap';
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
-    const localizer = BigCalendar.momentLocalizer(moment);
     this.state = {  };
   }
+
+
+
   render() { 
     return ( 
-      // <div style={{border: "1px solid black"}}>
-      //   {this.props.events.map((event, index) => {
-      //     return <div key = {index}>
-      //       <h1>{event.summary}</h1>
-      //       <h2>{event.start.dateTime}</h2>
-      //       <h2>{event.end.dateTime}</h2>
-      //     </div>
-      //   })}
-      // </div>
-      <div>
-        <BigCalendar localizer = {localizer} events={this.props.events}/>
-
+      <div style={{border: "1px solid black"}}>
+        {this.props.events.map((event, index) => {
+          return <div key = {index}>
+            <h1>{event.summary}</h1>
+            <h2>{event.start.dateTime}</h2>
+            <h2>{event.end.dateTime}</h2>
+          </div>
+        })}
       </div>
      );
   }
