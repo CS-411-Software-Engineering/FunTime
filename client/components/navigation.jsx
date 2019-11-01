d8b820632e63da32f0fec201b58766c6a82bf695
zzx <zzx@bu.edu>
2019-10-31 21:23:47 -0400

    button changed

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Nav } from 'react-bootstrap';
+import {Button, Navbar, Nav } from 'react-bootstrap';
+import "../../public/navigation.css"
+
 class Navigation extends Component {
   constructor(props) {
     super(props);
@@ -12,19 +14,41 @@ class Navigation extends Component {
     this.props.logOut();
   }
 
+  /*variant="pills"*/
   render() { 
+
     return ( 
-      <Nav variant="pills" defaultActiveKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <button id="signout_button" onClick= {this.handleSignoutClick}>Sign Out</button>
-        </Nav.Item>
-      </Nav>
+      <div>
+        <Navbar>
+          {/*<style type="text/css">
+            {`
+            .nav-link.active {
+              background-color: #ff6c00;
+              color: white;
+            }
+            `}
+          </style>*/}
+          <Nav defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <style type="text/css">
+                {`
+                .btn-design {
+                  background-color: #ff6c00;
+                  color: white;
+                }
+                `}
+              </style>
+              <Button variant = "design" id="signout_button" onClick= {this.handleSignoutClick}>Sign Out</Button>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
+      </div>
      );
   }
 }
