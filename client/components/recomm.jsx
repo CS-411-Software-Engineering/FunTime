62db6fc2f05fbb62b756eff925e9f39336ad4198
zzx <zzx@bu.edu>
2019-12-06 01:39:29 -0500

    Adding Event to Google Calendar

--- client/components/recomm.jsx
+++ client/components/recomm.jsx
@@ -2,14 +2,48 @@ import React, { Component } from 'react'
 import {Card, Button} from 'react-bootstrap';
 import './recomm.css';
 
+
 class Recomm extends Component{
+    constructor(props) {
+        super(props);
+        this.add = this.add.bind(this)
+    }
+    add(){
+        
+        var start = this.props.dateTime;
+        var localDate = new Date(start);
+        localDate.setHours(localDate.getHours() + 2);
+        var end = localDate.toISOString();
+        // console.log(end)
+        // console.log(start)
+        var event = {
+            'summary': this.props.title,
+            'start': {
+                // 'dateTime': '2019-12-13T20:00:00-07:00'
+                'dateTime': start
+              },
+            'end': {
+                'dateTime': end
+            }
+        }
+        var request = window.gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event
+          });
+        request.execute(function(event) {
+            console.log("HEREE")
+            console.log("EXECUTE event", event);
+            // appendPre('Event created: ' + event.htmlLink);
+          });
+        window.location.reload();
+    }
     render(){
         return(
             <Card className = "recomm" style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={this.props.img} />
                 <Card.Body>
                     <Card.Title>{this.props.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{this.props.time}</Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">{this.props.time}</Card.Subtitle>
                     <Card.Text>
                         {this.props.info}
                     </Card.Text>
@@ -21,7 +55,7 @@ class Recomm extends Component{
                         }
                         `}
                     </style>
-                    <Button variant="design" onClick = {this.props.add}>Add to Schedule</Button>
+                    <Button variant="design" onClick = {this.add}>Add to Schedule</Button>
                 </Card.Body>
             </Card>
         );
